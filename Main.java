a71c62c4c8abd8044466d2f14d76b5febab3af8f
32248480 <92283556+32248480@users.noreply.github.com>
2021-11-15 08:38:42 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+//(a),(b),
+class CRectangle{
+ protected int length;
+ protected int width;
+
+public CRectangle(int l,int w){
+ length=l;
+ width=w;
+}
+public String toString(){
+ String str="length="+length+", width="+width;
+ str+=", area="+length*width;
+ return str;
+}
+
+public String toString(int a,int b){
+ if(a>=b)
+ return super.toString();
+ else
+ return "Argument Error";
+}
+}
+
+public class Main{
+public static void main(String args[]){
+ CRectangle rect=new CRectangle(2,6);
+ System.out.println(rect.toString());
+ System.out.println(rect.toString(8,4));
+ System.out.println(rect.toString(3,5));
+}
+}
+
+//length=2, width=6, area=12
+//CRectangle@28a418fc
+//Argument Error
+
+//(c),
+//在(a)和(b)中所定義的兩個toString()method得以同時存在於一個類別內,是利用OOP中的「多載」。
